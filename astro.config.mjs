3f19c9b6803340d6c0de323241d94933d165dffc
Fedor Ivanenko <fiodor.ivanenko@gmail.com>
2023-04-04 16:39:33 +0100

    update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://catia-maninina.github.io',
+  site: 'https://fiodor8.github.io/',
+  base: 'catia-malinina/'
 });
